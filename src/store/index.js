ba469a9210516b835a24984cab577d0e99530cb1
Luiza <luizapereiratvd@gmail.com>
2019-11-22 13:49:11 -0300

    Adding reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,40 +1,7 @@
 import { createStore } from "redux";
 
-const INNITIAL_STATE = {
-  activeLesson: {},
-  activeModule: {},
-  modules: [
-    {
-      id: 1,
-      title: "Iniciando com React",
-      lessons: [
-        { id: 1, title: "Primeira aula" },
-        { id: 2, title: "Segunda aula" }
-      ]
-    },
-    {
-      id: 2,
-      title: "Aprendendo Redux",
-      lessons: [
-        { id: 3, title: "Terceira aula" },
-        { id: 4, title: "Quarta aula" }
-      ]
-    }
-  ]
-};
-//retorna como o state inicializa com a aplicação
-function reducer(state = INNITIAL_STATE, action) {
-  if (action.type === "TOGGLE_LESSON") {
-    return {
-      ...state,
-      activeLesson: action.lesson,
-      activeModule: action.module
-    };
-  }
+import rootReducer from "./reducers";
 
-  return state;
-}
-
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 export default store;
---
+++ src/store/reducers/course.js
@@ -0,0 +1,34 @@
+const INNITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        { id: 1, title: "Primeira aula" },
+        { id: 2, title: "Segunda aula" }
+      ]
+    },
+    {
+      id: 2,
+      title: "Aprendendo Redux",
+      lessons: [
+        { id: 3, title: "Terceira aula" },
+        { id: 4, title: "Quarta aula" }
+      ]
+    }
+  ]
+};
+
+//retorna como o state inicializa com a aplicação
+export default function course(state = INNITIAL_STATE, action) {
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeLesson: action.lesson,
+      activeModule: action.module
+    };
+  }
+  return state;
+}
